a4ccf789c302c23360405bcef526d37d1b189537
Catarina Silva <c.alexandracorreia@ua.pt>
2020-04-27 23:04:07 +0100

    Finish first script

--- script_1.m
+++ script_1.m
@@ -18,23 +18,26 @@ for l=1:size(lambda,2)
                 MPD = [MPD; mpd];
                 TT = [TT; tt];
             end
-            SEM = std(PL)/sqrt(length(PL));               % Standard Error
-            ts = tinv([0.025  0.975],length(PL)-1);      % T-Score
-            CIpl = mean(PL) + ts*SEM; % Confidence Intervals
             
-            SEM = std(APD)/sqrt(length(APD));               % Standard Error
-            ts = tinv([0.025  0.975],length(APD)-1);      % T-Score
-            CIapd = mean(APD) + ts*SEM; % Confidence Intervals
+            meanPL = mean(PL); 
+            nPL = size(PL, 1); 
+            semPL = std(PL) ./ sqrt(nPL); 
+
+            meanAPD = mean(APD); 
+            nAPD = size(APD, 1); 
+            semAPD = std(APD) ./ sqrt(nAPD); 
+
+            meanMPD = mean(MPD); 
+            nMPD = size(MPD, 1); 
+            semMPD = std(MPD) ./ sqrt(nMPD); 
+
+            meanTT = mean(TT); 
+            nTT = size(TT, 1); 
+            semTT = std(TT) ./ sqrt(nTT); 
+
+
+            disp([meanPL, 1.645*semPL, meanAPD, 1.645*semAPD, meanAPD, 1.645*semAPD, meanMPD, 1.645*semMPD, meanTT, 1.645*semTT])
             
-            SEM = std(MPD)/sqrt(length(MPD));               % Standard Error
-            ts = tinv([0.025  0.975],length(MPD)-1);      % T-Score
-            CImpd = mean(MPD) + ts*SEM; % Confidence Intervals
-            
-            SEM = std(TT)/sqrt(length(TT));               % Standard Error
-            ts = tinv([0.025  0.975],length(TT)-1);      % T-Score
-            CItt = mean(TT) + ts*SEM; % Confidence Intervals
-            
-            disp([lambda(l), C(c), F(f), CIpl, CIapd, CImpd, CItt])
         end
     end
 end
