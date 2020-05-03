5c9732c43d1eaa7f8136beb6c05d2b8b4026981f
Catarina Silva <c.alexandracorreia@ua.pt>
2020-05-03 17:42:38 +0100

    Update script1

--- script_1.m
+++ script_1.m
@@ -1,43 +1,40 @@
-lambda = [100, 200, 500, 1000];
-C = [2, 10];
-F = [100000, 10000];
-P = 1000;
+lambda1 = [100 200];    % Packet Rate 
+lambda2 = [500 1000];
+C = [2 10];                     % Connection Capacity 
+F = [100000 10000];             % Queue
+P = 1000;                       % Number of Transmitted Packets
+s = 10;                         % Number of Simulations
 
-
-for l=1:size(lambda,2)
-    for c = 1:size(C,2)
-        for f = 1:size(F,2)
-            PL = [];
-            APD = [];
-            MPD = [];
-            TT = [];
-            for i = 1:10
-                [pl, apd, mpd, tt] = Simulator1(lambda(l),C(c),F(f),P);
-                PL = [PL; pl];
-                APD = [APD; apd];
-                MPD = [MPD; mpd];
-                TT = [TT; tt];
+for c = 1:size(C, 2)
+    for f = 1:size(F, 2)
+        for l = 1:size(1, 2)
+            aPL = [];
+            aAD = [];
+            aMD = [];
+            aTT = [];
+            if C(c) == 2
+                lamda = lambda1;
+            else
+                lambda = lambda2;
+            end 
+            for i = 1:s
+                [PL , APD , MPD , TT] = Simulator1(lambda(l),C(c),F(f),P);
+                aPL = [aPL PL];
+                aAD = [aAD APD];
+                aMD = [aMD MPD];
+                aTT = [aTT TT];
             end
-            
-            meanPL = mean(PL); 
-            nPL = size(PL, 1); 
-            semPL = std(PL) ./ sqrt(nPL); 
 
-            meanAPD = mean(APD); 
-            nAPD = size(APD, 1); 
-            semAPD = std(APD) ./ sqrt(nAPD); 
-
-            meanMPD = mean(MPD); 
-            nMPD = size(MPD, 1); 
-            semMPD = std(MPD) ./ sqrt(nMPD); 
-
-            meanTT = mean(TT); 
-            nTT = size(TT, 1); 
-            semTT = std(TT) ./ sqrt(nTT); 
-
-
-            disp([meanPL, 1.645*semPL, meanAPD, 1.645*semAPD, meanMPD, 1.645*semMPD, meanTT, 1.645*semTT])
-            
+            packet_loss = mean(aPL) 
+            av_packet_delay = mean(aAD); 
+            max_packet_delay = mean(aMD); 
+            trans = mean(aTT);
+            packet_loss_conf = 1.645*(std(aPL)/sqrt(s))   
+            av_packet_delay_conf = 1.645*(std(aAD)/sqrt(s));  
+            max_packet_delay_conf = 1.645*(std(aMD)/sqrt(s));   
+            trans_conf = 1.645*(std(aTT)/sqrt(s));
         end
     end
 end
+
+
