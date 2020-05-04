11c0a107022d2ca1aefeef5de2ba1d46577fe66e
Catarina Silva <c.alexandracorreia@ua.pt>
2020-05-04 16:50:24 +0100

    Update scripts

--- average_packet_delay_mg1.m
+++ average_packet_delay_mg1.m
@@ -1,21 +1,35 @@
 L = [100, 200, 500, 1000];
-
-
-
-%função com c e l
-%estimar o b
-%devolve
-
 C = [2*10^6, 10*10^6];
 
 
 for l=1:size(L,2)
     W = [];
     for c = 1:size(C,2)
-        [w] = 1/(c/b);
+        [w] = mg1(c,l)
         W = [W;w];
     end
     disp = (W)
 end
+
+function avg = mg1(c, l)
+    p0 = 0.16;
+    p1 = 0.22;
+    p2 = 0.62;
+    n0 = 64*8;
+    n1 = 1518*8;
+    n2_aux = (1517+65)/2;
+    n2 = n2_aux*8;
+
+    b = p0*n0 + p1*n1 + p2*n2;
+
+    es = (1/(c/b));
+    esQ = es^2;
+
+    avg = (l*esQ)/(2(1-l*es));
+    
+end
+
+
+
 
 
--- script_1.m
+++ script_1.m
@@ -5,6 +5,7 @@ F = [100000 10000];             % Queue
 P = 1000;                       % Number of Transmitted Packets
 s = 10;                         % Number of Simulations
 
+result = [];
 for c = 1:size(C, 2)
     for f = 1:size(F, 2)
         for l = 1:size(1, 2)
@@ -25,14 +26,14 @@ for c = 1:size(C, 2)
                 aTT = [aTT TT];
             end
 
-            packet_loss = mean(aPL) 
-            av_packet_delay = mean(aAD); 
-            max_packet_delay = mean(aMD); 
-            trans = mean(aTT);
-            packet_loss_conf = 1.645*(std(aPL)/sqrt(s))   
-            av_packet_delay_conf = 1.645*(std(aAD)/sqrt(s));  
-            max_packet_delay_conf = 1.645*(std(aMD)/sqrt(s));   
-            trans_conf = 1.645*(std(aTT)/sqrt(s));
+            result(c,f,l,1) = mean(aPL) 
+            result(c,f,l,2) = mean(aAD); 
+            result(c,f,l,3) = mean(aMD); 
+            result(c,f,l,4) = mean(aTT);
+            result(c,f,l,5) = 1.645*(std(aPL)/sqrt(s))   
+            result(c,f,l,6) = 1.645*(std(aAD)/sqrt(s));  
+            result(c,f,l,7) = 1.645*(std(aMD)/sqrt(s));   
+            result(c,f,l,8) = 1.645*(std(aTT)/sqrt(s));
         end
     end
 end
